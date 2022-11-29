e5cf89bf8301304899331999d864d7aebf153357
rantanevich <r.antanevich@gmail.com>
2022-11-29 06:08:28 +0400

    configure pods routing between nodes

--- terraform/gcp_network.tf
+++ terraform/gcp_network.tf
@@ -24,3 +24,13 @@ resource "google_compute_router_nat" "main" {
   nat_ip_allocate_option             = "AUTO_ONLY"
   source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
 }
+
+resource "google_compute_route" "kubernetes" {
+  count = var.worker_nodes
+
+  name        = format("worker-%d-pods", count.index)
+  dest_range  = format(var.pods_network_template, count.index)
+  next_hop_ip = format(var.worker_network_ip_template, count.index)
+  network     = google_compute_network.main.name
+  priority    = 100
+}
